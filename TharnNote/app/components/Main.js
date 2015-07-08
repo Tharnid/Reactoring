7a0ccea60910daa11ae6cc0f35afd8bae22c86b1
Tharnid <jeffmsmithdev@gmail.com>
2015-07-08 10:37:28 -0400

    first deploy

---
+++ TharnNote/app/components/Main.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Main = React.createClass({
+   render: function() {
+       return (
+           <div>
+                Hello from React!!!
+           </div>
+       )
+   }
+});
+
+// Renders the file
+React.render(<Main />, document.getElementById('app'));
