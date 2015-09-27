f2954a95b3a1e7f2d4fe58515911eaca8db67de4
Tharnid <jeffmsmithdev@gmail.com>
2015-09-27 12:36:29 -0400

    button

---
+++ GSR/scripts/whatever.jsx
@@ -0,0 +1,9 @@
+var Babydoll = React.createClass({
+    render: function () {
+        return (
+            <button>What?</button>
+        )
+    }
+});
+
+React.render(<Babydoll />, document.getElementById("root"));
