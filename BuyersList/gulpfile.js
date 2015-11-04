6b700a4a8c97d1fd3a8c99c866b43a7dec3d16a0
Tharnid <jeffmsmithdev@gmail.com>
2015-11-04 07:46:08 -0500

    moving on

--- BuyersList/gulpfile.js
+++ BuyersList/gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var LiveServer = require('gulp-live-server');
 var browserSync = require('browser-sync');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var reactify = require('reactify');
 
 gulp.task('live-server',function(){
 	var server = new LiveServer('server/main.js');
@@ -12,4 +15,16 @@ gulp.task('serve', ['live-server'], function(){
 		proxy: "http://localhost:1974",
 		port: 7411
 	});
+});
+
+gulp.task('bundle',function() {
+	return browserify({
+		entries:'app/main.jsx',
+		debug: true,
+		
+	})
+	.transform(reactify)
+	.bundle()
+	.pipe(source('app.js'))
+	.pipe(gulp.dest('./.tmp'));
 });
